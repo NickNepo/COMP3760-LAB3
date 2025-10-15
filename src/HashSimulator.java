bbe02cfb80b40574b39b347d35558bf899e28fd6
Nick Nepo <nicknepo004@gmail.com>
2025-10-15 14:55:52 -0700

    Moving back home to desktop

--- src/HashSimulator.java
+++ src/HashSimulator.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+/**
+ * @author Nicholas Nepomuceno
+ * Student Num: A01431279
+ * @version 1.0
+ */
+public class HashSimulator
+{
+    public int[] runHashSimulation (String[] keys, int HTSize)
+    {
+        int result[] = new int[6];
 
-public class HashSimulator {
+        return result;
+    }
+
+    public int H1(String name, int HTSize)
+    {
+        int hashValue = 0;
+
+        return hashValue;
+    }
+    public int H2(String name, int HTSize)
+    {
+        int hashValue = 0;
+
+        return hashValue;
+    }
+
+    public int H3(String name, int HTSize)
+    {
+        int hashValue = 0;
+
+        return hashValue;
+    }
+
 }
